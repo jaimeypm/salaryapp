a622b1044967811c05643fc3f971566d56c1ad63
jaime <71741284+jaimeypm@users.noreply.github.com>
2021-04-06 18:43:45 -0700

    file dump

---
+++ platforms/android/app/src/main/assets/www/js/weather-location.js
@@ -0,0 +1,8 @@
+var weatherLocation =[
+    { title: 'Vancouver', path:'', desc:'British Columbia, Canada'},
+    { title: 'Calgary', path:'', desc:'Alberta, Canada'},
+    { title: 'Toronto', path:'', desc:'Ontario, Canada'},
+    { title: 'New York', path:'', desc:'New York, USA'},
+    { title: 'Los Angeles', path:'', desc:'California, USA'},
+    { title: 'Houston', path:'', desc:'Texas, USA'},
+];
