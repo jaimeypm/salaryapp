7dd0a4a124ef6a452f37045a362b11d927a4d3bd
verdi-pang <75467831+verdi-pang@users.noreply.github.com>
2021-04-07 23:49:46 -0700

    added splash screen configurations

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -33,11 +33,16 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
+
+        navigator.splashscreen.hide();
+
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+
+        
 
         console.log('Received Event: ' + id);
     }
